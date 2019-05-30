5fca17263dc75535331f949581d272f19d1d455a
Ryan Dillinger <rd.nss.services@gmail.com>
2019-05-30 14:36:45 -0500

    scaffolding and controller created for ProductType

--- Bangazon/Controllers/ProductTypeController.cs
+++ Bangazon/Controllers/ProductTypeController.cs
@@ -2,65 +2,152 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bangazon.Data;
 using Bangazon.Models;
-using Bangazon.Models.ProductViewModels;
-using System.Data.SqlClient;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Bangazon.Controllers
 {
-    public class ProductTypeController : Controller
+    public class ProductTypesController : Controller
     {
-        // GET: /<controller>/
-        private readonly IConfiguration _config;
+        private readonly ApplicationDbContext _context;
 
-        public ProductTypeController(IConfiguration config)
+        public ProductTypesController(ApplicationDbContext context)
         {
-            _config = config;
+            _context = context;
         }
-        public SqlConnection Connection
+
+        // GET: ProductTypes
+        public async Task<IActionResult> Index()
         {
-            get
+            return View(await _context.ProductType.ToListAsync());
+        }
+
+        // GET: ProductTypes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
             {
-                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+                return NotFound();
             }
-        }
-        // GET: Students
-        public ActionResult Index()
-        {
-            using (SqlConnection conn = Connection)
+
+            var productType = await _context.ProductType
+                .FirstOrDefaultAsync(m => m.ProductTypeId == id);
+            if (productType == null)
             {
-                conn.Open();
-                using (SqlCommand cmd = conn.CreateCommand())
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductTypeId,Label")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        // POST: ProductTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductTypeId,Label")] ProductType productType)
+        {
+            if (id != productType.ProductTypeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
                 {
-                    cmd.CommandText = @"
-                            SELECT s.ProductTypeId,
-                                s.Label,
-                            FROM ProductType s
-                        ";
-                    SqlDataReader reader = cmd.ExecuteReader();
-
-                    List<ProductType> productTypes = new List<ProductType>();
-                    while (reader.Read())
-                    {
-                        ProductType productType = new ProductType
-                        {
-                            ProductTypeId = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Label = reader.GetString(reader.GetOrdinal("Label")),
-                        };
-
-                        productTypes.Add(productType);
-                    }
-
-                    reader.Close();
-                    return View(productTypes);
-
+                    _context.Update(productType);
+                    await _context.SaveChangesAsync();
                 }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductTypeExists(productType.ProductTypeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
             }
+            return View(productType);
+        }
+
+        // GET: ProductTypes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductType
+                .FirstOrDefaultAsync(m => m.ProductTypeId == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+
+        // POST: ProductTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productType = await _context.ProductType.FindAsync(id);
+            _context.ProductType.Remove(productType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductTypeExists(int id)
+        {
+            return _context.ProductType.Any(e => e.ProductTypeId == id);
         }
     }
 }
